790f468c0719366563c716605313932730aa0ce2
hyperkai <kazya.ito.dream@gmail.com>
2021-03-31 09:31:15 +0900

    Update createsuperuser.py

--- aws-blog/createsuperuser.py
+++ aws-blog/createsuperuser.py
@@ -1,3 +1,3 @@
 from django.contrib.auth.models import User 
 User.objects.filter(username='admin').exists() or \
-User.objects.create_superuser('admin', 'admin@admin.com', 'admin')
+User.objects.create_superuser('admin', 'admin@admin.com', 'adminpw')
