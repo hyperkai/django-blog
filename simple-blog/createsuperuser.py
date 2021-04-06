8fbe19e1725698c801ae0f742ee7663815e793e3
hyperkai <kazya.ito.dream@gmail.com>
2021-04-07 02:13:07 +0900

    Update createsuperuser.py

--- simple-blog/createsuperuser.py
+++ simple-blog/createsuperuser.py
@@ -1,3 +1,3 @@
 from django.contrib.auth.models import User 
-User.objects.filter(username='adminn').exists() or \
-User.objects.create_superuser('adminn', 'admin@admin.com', 'adminnpw')
+User.objects.filter(username='admin').exists() or \
+User.objects.create_superuser('admin', 'admin@admin.com', 'adminpw')
